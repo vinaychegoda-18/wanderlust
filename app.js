b639afa177106a10f72a365cc40023ab26e9a632
Vinay Chegonda <vinaychegoda123@gmail.com>
2026-02-08 09:05:39 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const sessionOptions={
   store,
   secret:process.env.SECRET,
   resave:false,
-  saveUninitialized:true,
+  saveUninitialized:false,
   cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
